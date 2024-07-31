8eca2e727d20435601f52c69cd02942f4e4df0be
L Suzuki <lehsuzuki@gmail.com>
2024-08-01 10:42:14 +1200

    print errors

--- .buildkite/steps/annotations/annotations.sh
+++ .buildkite/steps/annotations/annotations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # set explanation: https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
-# set -euxo pipefail # print executed commands to the terminal
-set -euo pipefail # don't print executed commands to the terminal
+set -euxo pipefail # print executed commands to the terminal
+# set -euo pipefail # don't print executed commands to the terminal
 
 # source shared functions
 . .buildkite/assets/functions.sh;
